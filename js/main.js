7565aed2e4fa2d1d50510bb2a6e9a4333f6249f0
aliceandherdolls <alicjagorkiewicz92@gmail.com>
2022-07-18 15:38:57 +0200

    array

---
+++ js/main.js
@@ -0,0 +1,21 @@
+let num1 = 30;
+let num2 = 4;
+let num3 = 3;
+let num4 = 14;
+let num5 = 4;
+
+let myArray = [num1, num2, num3, num4, num5];
+
+/* indexOf(par1); */
+
+function firstelement(array) {
+    return array[0];
+}
+
+console.log(firstelement(myArray));
+
+
+
+
+
+
